b7295c8600fd8354145c107d2522a578e147c06f
Gabor Czene <gabor@czene.co.uk>
2020-03-04 19:40:54 +0000

    feat: DeckView

---
+++ src/components/DeckView/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
+import UdaButton from '../UdaButton';
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingTop: 20,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: '700',
+    marginTop: 30,
+    paddingVertical: 15,
+  },
+  subTitle: {
+    paddingVertical: 15,
+    fontSize: 24,
+  },
+})
+const initialCard = {
+  title: '',
+  questions: []
+}
+export default ({card = initialCard}) => {
+  const { title, questions } = initialCard;
+  return (
+    <View style={style.container}>
+      <Text style={style.title}>{title}</Text>
+      <Text style={style.subTitle}>{questions.length} cards</Text>
+      <View>
+        <UdaButton>Add card</UdaButton>
+        <UdaButton secondary>Start Quiz</UdaButton>
+      </View>
+    </View>
+  );
+};
---
+++ src/components/UdaButton/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Text, TouchableOpacity, StyleSheet, View } from "react-native";
+
+
+const btnStyle = StyleSheet.create({
+  btn: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    margin: 5,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+  primary: {
+    backgroundColor: '#FF6A70',
+  },
+  secondary: {
+    borderColor: '#2F1627',
+    borderWidth: 1,
+  },
+});
+
+export default ({text = 'no text passed', secondary, style = {}, children}) => {
+  const predefinedStyle = secondary ? btnStyle.secondary : btnStyle.primary;
+
+  const styles = [btnStyle.btn, predefinedStyle, style];
+
+  return (
+    <TouchableOpacity style={styles}>
+      <Text style={btnStyle.text}>{children}</Text>
+    </TouchableOpacity>
+  );
+}
